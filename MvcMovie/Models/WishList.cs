73759241f20d1978cc812a7cf9d48af04dd9a25d
heyhey <fruitsensatie@live.nl>
2017-08-11 21:04:49 +0200

    Begin tutorial basic MVC

---
+++ MvcMovie/Models/WishList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class WishList
+    {
+        public int ID { get; set; }
+
+        public List<Movie> favItems { get; set; }
+    }
+}
