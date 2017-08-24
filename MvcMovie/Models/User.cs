43f24b0a33d904780b43451fcd2a51313a37c014
heyhey <fruitsensatie@live.nl>
2017-08-24 20:38:23 +0200

    Login werkt basic zonder role

---
+++ MvcMovie/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        
+        [StringLength(60, MinimumLength = 4)]
+        [Required]
+        public string Email { get; set; }
+
+        [StringLength(60, MinimumLength = 8)]
+        [Required]
+        public string Password { get; set; }
+
+        public Role Role { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("User, Email, Password, Role", Email, Password, Role);
+        }
+    }
+}
---
+++ MvcMovie/Models/WishListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class WishListViewModel
+    {
+        public List<Movie> favItems;
+        public SelectList items;
+
+        public WishListViewModel()
+    {
+            favItems = new List<Movie>();
+    }
+
+    }
+}
+
+
